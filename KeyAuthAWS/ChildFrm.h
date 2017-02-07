15a0d4cbf44b0d36fc2423a42b3b3d2e1bb6b0e4
benjaminer82 <benjamine_r@hotmail.com>
2017-02-07 23:47:47 +0000

    Initialized

---
+++ KeyAuthAWS/ChildFrm.h
@@ -0,0 +1,51 @@
+// This MFC Samples source code demonstrates using MFC Microsoft Office Fluent User Interface 
+// (the "Fluent UI") and is provided only as referential material to supplement the 
+// Microsoft Foundation Classes Reference and related electronic documentation 
+// included with the MFC C++ library software.  
+// License terms to copy, use or distribute the Fluent UI are available separately.  
+// To learn more about our Fluent UI licensing program, please visit 
+// http://go.microsoft.com/fwlink/?LinkId=238214.
+//
+// Copyright (C) Microsoft Corporation
+// All rights reserved.
+
+// ChildFrm.h : interface of the CChildFrame class
+//
+
+#pragma once
+#include "ChildView.h"
+
+class CChildFrame : public CMDIChildWndEx
+{
+	DECLARE_DYNCREATE(CChildFrame)
+public:
+	CChildFrame();
+
+// Attributes
+public:
+
+// Operations
+public:
+
+// Overrides
+	public:
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+	virtual BOOL OnCmdMsg(UINT nID, int nCode, void* pExtra, AFX_CMDHANDLERINFO* pHandlerInfo);
+
+// Implementation
+public:
+	// view for the client area of the frame.
+	CChildView m_wndView;
+	virtual ~CChildFrame();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+// Generated message map functions
+protected:
+	afx_msg void OnFileClose();
+	afx_msg void OnSetFocus(CWnd* pOldWnd);
+	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
+	DECLARE_MESSAGE_MAP()
+};
---
+++ KeyAuthAWS/KeyAuthAWS.h
@@ -0,0 +1,54 @@
+// This MFC Samples source code demonstrates using MFC Microsoft Office Fluent User Interface 
+// (the "Fluent UI") and is provided only as referential material to supplement the 
+// Microsoft Foundation Classes Reference and related electronic documentation 
+// included with the MFC C++ library software.  
+// License terms to copy, use or distribute the Fluent UI are available separately.  
+// To learn more about our Fluent UI licensing program, please visit 
+// http://go.microsoft.com/fwlink/?LinkId=238214.
+//
+// Copyright (C) Microsoft Corporation
+// All rights reserved.
+
+// KeyAuthAWS.h : main header file for the KeyAuthAWS application
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"       // main symbols
+
+
+// CKeyAuthAWSApp:
+// See KeyAuthAWS.cpp for the implementation of this class
+//
+
+class CKeyAuthAWSApp : public CWinAppEx
+{
+public:
+	CKeyAuthAWSApp();
+
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// Implementation
+protected:
+	HMENU  m_hMDIMenu;
+	HACCEL m_hMDIAccel;
+
+public:
+	UINT  m_nAppLook;
+	virtual void PreLoadState();
+	virtual void LoadCustomState();
+	virtual void SaveCustomState();
+
+	afx_msg void OnAppAbout();
+	afx_msg void OnFileNew();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CKeyAuthAWSApp theApp;
